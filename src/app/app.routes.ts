4717a65e89cd545f52b3375a8ad471e8eee9cfbd
Shako The developer Giorgashvili <150827256+shako03@users.noreply.github.com>
2025-07-11 14:25:22 +0400

    cmt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      {path: '', redirectTo: '/home', pathMatch: 'full'},
+             
+      {
+        path: 'home',
+        loadComponent: () => import('./home/home.component').then(c => c.HomeComponent)
+      },
+
+      {
+            path: 'header',
+            loadComponent: () => import('./header/header.component').then(c => c.HeaderComponent)
+      },
+
+      {
+            path: 'footer',
+            loadComponent: () => import('./footer/footer.component').then(c => c.FooterComponent)
+      }
+
+];
