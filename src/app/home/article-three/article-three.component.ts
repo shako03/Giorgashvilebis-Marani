dbfaaa8936d3fde55690a115987f68aa27b0bc1b
Shako The developer Giorgashvili <150827256+shako03@users.noreply.github.com>
2025-07-14 16:37:18 +0400

    ng commit

---
+++ src/app/home/article-three/article-three.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-three',
+  imports: [],
+  templateUrl: './article-three.component.html',
+  styleUrl: './article-three.component.scss'
+})
+export class ArticleThreeComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ArticleOneComponent } from "./article-one/article-one.component";
 import { ArticleTwoComponent } from './article-two/article-two.component';
+import { ArticleThreeComponent } from "./article-three/article-three.component";
 
 @Component({
   selector: 'app-home',
-  imports: [ArticleOneComponent, ArticleTwoComponent],
+  imports: [ArticleOneComponent, ArticleTwoComponent, ArticleThreeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
