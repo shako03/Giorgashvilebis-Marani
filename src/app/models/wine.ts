f0129adc868e68a95db549a0c6abdad3ce823898
Shako The developer Giorgashvili <150827256+shako03@users.noreply.github.com>
2025-07-15 16:34:34 +0400

    msg

---
+++ src/app/models/wine.ts
@@ -0,0 +1,22 @@
+export class wine {
+      id?:number;
+      name!:string;
+      type!:string;
+      year?:number;
+      price?:number;
+      isAvailable:boolean|undefined;
+      origin!:string;
+      img!:string;
+}
+
+
+
+//     {
+//       id: 1,
+//       name: 'საფერავი',
+//       type: 'წითელი',
+//       year: 2020,
+//       price: 25,
+//       isAvailable: true,
+//       origin: 'კახეთი'
+//     },
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import { wine } from '../models/wine';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-shop',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
 })
 export class ShopComponent {
 
-  wines: any[] = [
+  wines: wine[] = [
     {
       id: 1,
       name: 'საფერავი',
@@ -16,7 +18,8 @@ export class ShopComponent {
       year: 2020,
       price: 25,
       isAvailable: true,
-      origin: 'კახეთი'
+      origin: 'კახეთი',
+      img:"https://cgw.ge/wp-content/uploads/2022/12/saperavi-min.png"
     },
     {
       id: 2,
@@ -25,9 +28,50 @@ export class ShopComponent {
       year: 2021,
       price: 18,
       isAvailable: false,
-      origin: 'იმერეთი'
+      origin: 'კახეთი',
+      img:"https://ktwshop.ge/598-large_default/%E1%83%A0%E1%83%A5%E1%83%90%E1%83%AC%E1%83%98%E1%83%97%E1%83%94%E1%83%9A%E1%83%98.jpg"
+    },
+    {
+      id: 3,
+      name: 'რქაწითელი როზე',
+      type: 'წითელი',
+      year: 2022,
+      price: 27,
+      isAvailable: false,
+      origin: 'კახეთი',
+      img:"https://ktwshop.ge/691-large_default/%E1%83%98%E1%83%90%E1%83%A1-%E1%83%95%E1%83%90%E1%83%A0%E1%83%93%E1%83%98.jpg"
+    },
+    {
+      id: 1,
+      name: 'საფერავი',
+      type: 'წითელი',
+      year: 2020,
+      price: 25,
+      isAvailable: true,
+      origin: 'კახეთი',
+      img:"https://cgw.ge/wp-content/uploads/2022/12/saperavi-min.png"
+    },
+    {
+      id: 2,
+      name: 'რქაწითელი',
+      type: 'თეთრი',
+      year: 2021,
+      price: 18,
+      isAvailable: false,
+      origin: 'კახეთი',
+      img:"https://ktwshop.ge/598-large_default/%E1%83%A0%E1%83%A5%E1%83%90%E1%83%AC%E1%83%98%E1%83%97%E1%83%94%E1%83%9A%E1%83%98.jpg"
+    },
+    {
+      id: 3,
+      name: 'რქაწითელი როზე',
+      type: 'წითელი',
+      year: 2022,
+      price: 27,
+      isAvailable: false,
+      origin: 'კახეთი',
+      img:"https://ktwshop.ge/691-large_default/%E1%83%98%E1%83%90%E1%83%A1-%E1%83%95%E1%83%90%E1%83%A0%E1%83%93%E1%83%98.jpg"
     }
-    
+
   ];
 
 
