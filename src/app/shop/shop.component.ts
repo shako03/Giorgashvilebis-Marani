bdd19422512a46b99854b03161a97ef38c8f0be4
Shako The developer Giorgashvili <150827256+shako03@users.noreply.github.com>
2025-07-18 23:19:20 +0400

    msg

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -42,24 +42,24 @@ export class ShopComponent {
       img:"https://ktwshop.ge/691-large_default/%E1%83%98%E1%83%90%E1%83%A1-%E1%83%95%E1%83%90%E1%83%A0%E1%83%93%E1%83%98.jpg"
     },
     {
-      id: 1,
-      name: 'საფერავი',
-      type: 'წითელი',
+      id: 4,
+      name: 'ქისი',
+      type: 'თეთრი',
       year: 2020,
-      price: 25,
+      price: 22,
       isAvailable: true,
       origin: 'კახეთი',
-      img:"https://cgw.ge/wp-content/uploads/2022/12/saperavi-min.png"
+      img:"https://cgw.ge/wp-content/uploads/2022/12/kisi-min.png"
     },
     {
-      id: 2,
-      name: 'რქაწითელი',
-      type: 'თეთრი',
-      year: 2021,
-      price: 18,
+      id: 5,
+      name: 'ალუბლის მიქსი',
+      type: 'წითელი',
+      year: 2022,
+      price: 32,
       isAvailable: false,
       origin: 'კახეთი',
-      img:"https://ktwshop.ge/598-large_default/%E1%83%A0%E1%83%A5%E1%83%90%E1%83%AC%E1%83%98%E1%83%97%E1%83%94%E1%83%9A%E1%83%98.jpg"
+      img:"https://8000vintages.ge/images/thumbnails/1200/1800/detailed/16/chigogidze_cherry_wine_nlnl.png"
     },
     {
       id: 3,
