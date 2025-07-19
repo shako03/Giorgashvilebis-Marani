4aaf707cedb74c396510cd681aa532baf8a612d3
Shako The developer Giorgashvili <150827256+shako03@users.noreply.github.com>
2025-07-19 15:19:38 +0400

    ng s about-3

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Article1AboutComponent } from "./article-1-about/article-1-about.component";
+import { Article2AboutComponent } from "./article-2-about/article-2-about.component";
 
 @Component({
   selector: 'app-about',
-  imports: [Article1AboutComponent],
+  imports: [Article1AboutComponent, Article2AboutComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
---
+++ src/app/about/article-3-about/article-3-about.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-3-about',
+  imports: [],
+  templateUrl: './article-3-about.component.html',
+  styleUrl: './article-3-about.component.scss'
+})
+export class Article3AboutComponent {
+
+}
