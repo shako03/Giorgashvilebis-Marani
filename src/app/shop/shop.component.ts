6fdf667de64dee641e1f703d43285abc71fe821a
Shako The developer Giorgashvili <150827256+shako03@users.noreply.github.com>
2025-07-19 11:42:59 +0400

    msg

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import { wine } from '../models/wine';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shop',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
 })
 export class ShopComponent {
 
   selectedType: string | null = null;
-  
+  minPrice: number | null = null;
+  maxPrice: number | null = null;
+
+
   wines: wine[] = [
     {
       id: 1,
@@ -21,7 +25,7 @@ export class ShopComponent {
       price: 25,
       isAvailable: true,
       origin: 'კახეთი',
-      img:"https://cgw.ge/wp-content/uploads/2022/12/saperavi-min.png"
+      img: "https://cgw.ge/wp-content/uploads/2022/12/saperavi-min.png"
     },
     {
       id: 2,
@@ -31,7 +35,7 @@ export class ShopComponent {
       price: 18,
       isAvailable: false,
       origin: 'კახეთი',
-      img:"https://ktwshop.ge/598-large_default/%E1%83%A0%E1%83%A5%E1%83%90%E1%83%AC%E1%83%98%E1%83%97%E1%83%94%E1%83%9A%E1%83%98.jpg"
+      img: "https://ktwshop.ge/598-large_default/%E1%83%A0%E1%83%A5%E1%83%90%E1%83%AC%E1%83%98%E1%83%97%E1%83%94%E1%83%9A%E1%83%98.jpg"
     },
     {
       id: 3,
@@ -41,7 +45,7 @@ export class ShopComponent {
       price: 27,
       isAvailable: false,
       origin: 'კახეთი',
-      img:"https://ktwshop.ge/691-large_default/%E1%83%98%E1%83%90%E1%83%A1-%E1%83%95%E1%83%90%E1%83%A0%E1%83%93%E1%83%98.jpg"
+      img: "https://ktwshop.ge/691-large_default/%E1%83%98%E1%83%90%E1%83%A1-%E1%83%95%E1%83%90%E1%83%A0%E1%83%93%E1%83%98.jpg"
     },
     {
       id: 4,
@@ -51,7 +55,7 @@ export class ShopComponent {
       price: 22,
       isAvailable: true,
       origin: 'კახეთი',
-      img:"https://cgw.ge/wp-content/uploads/2022/12/kisi-min.png"
+      img: "https://cgw.ge/wp-content/uploads/2022/12/kisi-min.png"
     },
     {
       id: 5,
@@ -61,7 +65,7 @@ export class ShopComponent {
       price: 32,
       isAvailable: false,
       origin: 'კახეთი',
-      img:"https://8000vintages.ge/images/thumbnails/1200/1800/detailed/16/chigogidze_cherry_wine_nlnl.png"
+      img: "https://8000vintages.ge/images/thumbnails/1200/1800/detailed/16/chigogidze_cherry_wine_nlnl.png"
     },
     {
       id: 3,
@@ -71,20 +75,26 @@ export class ShopComponent {
       price: 27,
       isAvailable: false,
       origin: 'კახეთი',
-      img:"https://ktwshop.ge/691-large_default/%E1%83%98%E1%83%90%E1%83%A1-%E1%83%95%E1%83%90%E1%83%A0%E1%83%93%E1%83%98.jpg"
+      img: "https://ktwshop.ge/691-large_default/%E1%83%98%E1%83%90%E1%83%A1-%E1%83%95%E1%83%90%E1%83%A0%E1%83%93%E1%83%98.jpg"
     }
 
   ];
 
-    get filteredWines(): wine[] {
-    if (!this.selectedType) {
-      return this.wines;
-    }
-    return this.wines.filter(wine => wine.type === this.selectedType);
-  }
+get filteredWines(): wine[] {
+  return this.wines.filter(wine => {
+    const byType = !this.selectedType || wine.type === this.selectedType;
+    const byMin = this.minPrice == null || wine.price >= this.minPrice;
+    const byMax = this.maxPrice == null || wine.price <= this.maxPrice;
+    return byType && byMin && byMax;
+  });
+}
 
   setFilter(type: string | null) {
     this.selectedType = type;
+  }
+
+  setPriceFilter(price: number | null) {
+    this.maxPrice = price;
   }
 
 }
