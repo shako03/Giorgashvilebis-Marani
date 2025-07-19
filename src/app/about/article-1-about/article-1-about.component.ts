9d88e7daac2217e99dc3f9ab27e9f5b35e4db67e
Shako The developer Giorgashvili <150827256+shako03@users.noreply.github.com>
2025-07-19 14:29:55 +0400

    msg

---
+++ src/app/about/article-1-about/article-1-about.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-1-about',
+  imports: [],
+  templateUrl: './article-1-about.component.html',
+  styleUrl: './article-1-about.component.scss'
+})
+export class Article1AboutComponent {
+
+}
