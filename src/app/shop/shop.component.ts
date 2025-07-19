1f3df76aa465566b99ec84eb4f7019d159920679
Shako The developer Giorgashvili <150827256+shako03@users.noreply.github.com>
2025-07-19 12:33:39 +0400

    msg

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { wine } from '../models/wine';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-shop',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,RouterModule],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
 })
@@ -68,7 +69,7 @@ export class ShopComponent {
       img: "https://8000vintages.ge/images/thumbnails/1200/1800/detailed/16/chigogidze_cherry_wine_nlnl.png"
     },
     {
-      id: 3,
+      id: 6,
       name: 'რქაწითელი როზე',
       type: 'წითელი',
       year: 2022,
@@ -76,8 +77,37 @@ export class ShopComponent {
       isAvailable: false,
       origin: 'კახეთი',
       img: "https://ktwshop.ge/691-large_default/%E1%83%98%E1%83%90%E1%83%A1-%E1%83%95%E1%83%90%E1%83%A0%E1%83%93%E1%83%98.jpg"
-    }
-
+    },
+     {
+    id: 7,
+    name: 'ცოლიკოური',
+    type: 'თეთრი',
+    year: 2021,
+    price: 20,
+    isAvailable: true,
+    origin: 'იმერეთი',
+    img: "https://ktwshop.ge/677-large_default/%E1%83%AA%E1%83%9D%E1%83%9A%E1%83%98%E1%83%99%E1%83%90%E1%83%A3%E1%83%A0%E1%83%98.jpg"
+  },
+  {
+    id: 8,
+    name: 'მერანი',
+    type: 'წითელი',
+    year: 2020,
+    price: 30,
+    isAvailable: true,
+    origin: 'სამეგრელო',
+    img: "https://ktwshop.ge/509-home_default/%E1%83%A8%E1%83%90%E1%83%95%E1%83%99%E1%83%90%E1%83%9E%E1%83%98%E1%83%A2%E1%83%9D.jpg"
+  },
+   {
+    id: 9,
+    name: 'თელიანი ველი საფერავი',
+    type: 'შავი',
+    year: 2021,
+    price: 35,
+    isAvailable: true,
+    origin: 'კახეთი',
+    img: "https://alcorium-store.ge/images/detailed/3/%E1%83%97%E1%83%94%E1%83%9A%E1%83%98%E1%83%90%E1%83%9C%E1%83%98_%E1%83%95%E1%83%94%E1%83%9A%E1%83%98_%E1%83%A1%E1%83%90%E1%83%A4%E1%83%94%E1%83%A0%E1%83%90%E1%83%95%E1%83%98_0.75%E1%83%9A.png"
+  }
   ];
 
   get filteredWines(): wine[] {
---
+++ src/app/wine-details/wine-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wine-details',
+  imports: [],
+  templateUrl: './wine-details.component.html',
+  styleUrl: './wine-details.component.scss'
+})
+export class WineDetailsComponent {
+
+}
