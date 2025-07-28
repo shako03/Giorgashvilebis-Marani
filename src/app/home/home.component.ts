e58a42fb13db382abc80a26bb76b4540690136ac
Shako The developer Giorgashvili <150827256+shako03@users.noreply.github.com>
2025-07-28 12:52:34 +0400

    msg

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,13 +2,27 @@ import { Component } from '@angular/core';
 import { ArticleOneComponent } from "./article-one/article-one.component";
 import { ArticleTwoComponent } from './article-two/article-two.component';
 import { ArticleThreeComponent } from "./article-three/article-three.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [ArticleOneComponent, ArticleTwoComponent, ArticleThreeComponent],
+  imports: [ArticleOneComponent, ArticleTwoComponent, ArticleThreeComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  showPopup = false;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.showPopup = true;
+    }, 2000); // 2 წამი
+  }
+
+  closePopup() {
+    this.showPopup = false;
+  }
+
 
 }
