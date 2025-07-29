b8298609b7ea7864cf4289ab8d0554c8915684c1
Shako The developer Giorgashvili <150827256+shako03@users.noreply.github.com>
2025-07-29 13:19:49 +0400

    msg

---
+++ src/app/about/article-photos/article-photos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-photos',
+  imports: [],
+  templateUrl: './article-photos.component.html',
+  styleUrl: './article-photos.component.scss'
+})
+export class ArticlePhotosComponent {
+
+}
