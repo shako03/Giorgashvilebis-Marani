a805bca982471642f5a5e396fb2ac6de68f6d529
Shako The developer Giorgashvili <150827256+shako03@users.noreply.github.com>
2025-07-29 13:58:35 +0400

    msg

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,10 +3,11 @@ import { Article1AboutComponent } from "./article-1-about/article-1-about.compon
 import { Article2AboutComponent } from "./article-2-about/article-2-about.component";
 import { Article3AboutComponent } from "./article-3-about/article-3-about.component";
 import { SliderComponent } from "../home/slider/slider.component";
+import { ArticlePhotosComponent } from "./article-photos/article-photos.component";
 
 @Component({
   selector: 'app-about',
-  imports: [Article1AboutComponent, Article2AboutComponent, Article3AboutComponent, SliderComponent],
+  imports: [Article1AboutComponent, Article2AboutComponent, Article3AboutComponent, SliderComponent, ArticlePhotosComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
