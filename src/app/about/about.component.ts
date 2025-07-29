750c7ce690aea1ce000aa45f6f32c75aede98460
Shako The developer Giorgashvili <150827256+shako03@users.noreply.github.com>
2025-07-29 13:19:14 +0400

    msg

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Article1AboutComponent } from "./article-1-about/article-1-about.component";
 import { Article2AboutComponent } from "./article-2-about/article-2-about.component";
 import { Article3AboutComponent } from "./article-3-about/article-3-about.component";
+import { SliderComponent } from "../home/slider/slider.component";
 
 @Component({
   selector: 'app-about',
-  imports: [Article1AboutComponent, Article2AboutComponent, Article3AboutComponent],
+  imports: [Article1AboutComponent, Article2AboutComponent, Article3AboutComponent, SliderComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
