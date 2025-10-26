3ff30a3129c2aaa91bbf9551f04581d77fecafad
Shako The developer Giorgashvili <150827256+shako03@users.noreply.github.com>
2025-10-26 12:27:37 +0400

    msg

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -108,7 +108,68 @@ export class ShopComponent {
       isAvailable: true,
       origin: 'კახეთი',
       img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2_nDGx2wnwasUUiDsKD9q1q8Kz2KL1m4REQ&s"
-    }
+    },
+    {
+      id: 10,
+      name: 'ცოლიკოური',
+      type: 'თეთრი',
+      year: 2021,
+      price: 20,
+      isAvailable: true,
+      origin: 'იმერეთი',
+      img: "https://ktwshop.ge/677-large_default/%E1%83%AA%E1%83%9D%E1%83%9A%E1%83%98%E1%83%99%E1%83%90%E1%83%A3%E1%83%A0%E1%83%98.jpg"
+    },
+    {
+      id: 11,
+      name: 'მერანი',
+      type: 'წითელი',
+      year: 2020,
+      price: 30,
+      isAvailable: true,
+      origin: 'სამეგრელო',
+      img: "https://ktwshop.ge/509-home_default/%E1%83%A8%E1%83%90%E1%83%95%E1%83%99%E1%83%90%E1%83%9E%E1%83%98%E1%83%A2%E1%83%9D.jpg"
+    },
+    {
+      id: 12,
+      name: 'თელიანი ველი საფერავი',
+      type: 'შავი',
+      year: 2021,
+      price: 35,
+      isAvailable: true,
+      origin: 'კახეთი',
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2_nDGx2wnwasUUiDsKD9q1q8Kz2KL1m4REQ&s"
+    },
+    {
+      id: 13,
+      name: 'ცოლიკოური',
+      type: 'თეთრი',
+      year: 2021,
+      price: 20,
+      isAvailable: true,
+      origin: 'იმერეთი',
+      img: "https://ktwshop.ge/677-large_default/%E1%83%AA%E1%83%9D%E1%83%9A%E1%83%98%E1%83%99%E1%83%90%E1%83%A3%E1%83%A0%E1%83%98.jpg"
+    },
+    {
+      id: 14,
+      name: 'მერანი',
+      type: 'წითელი',
+      year: 2020,
+      price: 30,
+      isAvailable: true,
+      origin: 'სამეგრელო',
+      img: "https://ktwshop.ge/509-home_default/%E1%83%A8%E1%83%90%E1%83%95%E1%83%99%E1%83%90%E1%83%9E%E1%83%98%E1%83%A2%E1%83%9D.jpg"
+    },
+    {
+      id: 15,
+      name: 'თელიანი ველი საფერავი',
+      type: 'შავი',
+      year: 2021,
+      price: 35,
+      isAvailable: true,
+      origin: 'კახეთი',
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2_nDGx2wnwasUUiDsKD9q1q8Kz2KL1m4REQ&s"
+    },
+    
   ];
 
   
